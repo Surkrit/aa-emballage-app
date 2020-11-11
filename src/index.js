4ee65ee1064b69a68209358ba72e996e9f4e94c1
Katja H. Kjer <Kalle3008@hotmail.com>
2020-11-11 12:29:19 +0100

    En anelse mappestruktur oprettet, startet på DropDown komponent, er statisk, mangler js til at gøre den clickable

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import DropDown from './components/DropDown';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DropDown />
   </React.StrictMode>,
   document.getElementById('root')
 );
