69cfd6ac3602adefe715a4fe86dd6cf9e5f0de51
Katja H. Kjer <Kalle3008@hotmail.com>
2020-11-11 15:29:48 +0100

    Dropdown lavet så input er via props

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,71 +1,36 @@
-import '../style/App.css';
+import "../style/App.css";
 
 function DropDown() {
   return (
     <div className="dropdown">
       <button className="enhed">+ Enhed</button>
-        <form className="form-enhed" action="/action_page.php">
-          <div className="form-input">
-            <input type="checkbox" name="t5rfidcc"/>
-            <label for="t5"> T5 RFID CC</label>
-          </div>
-          <div className="form-input">
-            <input type="checkbox" name="hylder"/>
-            <label for="hylder"> HYLDER</label>
-          </div>
-          <div className="form-input">
-            <input type="checkbox" name="forl"/>
-            <label for="forl"> FORL.</label>
-          </div>
-          <div className="form-input">
-            <input type="checkbox" name="rfidcc"/>
-            <label for="rfidcc"> RFID CC</label>
-          </div>
-          <div className="form-input">
-            <input type="checkbox" name="halfrfidcc"/>
-            <label for="halfrfidcc"> ½ RFID CC</label>
-          </div>
-          <div className="form-input">
-            <input type="checkbox" name="halfhylde"/>
-            <label for="halfhylde"> ½ hylde</label>
-          </div>
-          <div className="form-input">
-            <input type="checkbox" name="halfcc"/>
-            <label for="halfcc"> ½ CC</label>
-          </div>
-          <div className="form-input">
-            <input type="checkbox" name="dshalfcc"/>
-            <label for="dshalfcc"> DS ½CC</label>
-          </div>
-          <div className="form-input">
-            <input type="checkbox" name="dshalfhylde"/>
-            <label for="dshalfhylde"> DS ½HYLDE</label>
-          </div>
-          <div className="form-input">
-            <input type="checkbox" name="eupl"/>
-            <label for="eupl"> EUPL</label>
-          </div>
-          <div className="form-input">
-            <input type="checkbox" name="halfpll"/>
-            <label for="halfpll"> ½ PLL</label>
-          </div>
-          <div className="form-input">
-            <input type="checkbox" name="quartpll"/>
-            <label for="quartpll"> ¼ PLL</label>
-          </div>
-          <div className="form-input">
-            <input type="checkbox" name="cc"/>
-            <label for="cc"> CC</label>
-          </div>
-          <div className="form-input">
-            <input type="checkbox" name="sojleror"/>
-            <label for="sojleror"> SØJLERØR</label>
-          </div>
-        </form>
+      <form className="form-enhed" action="/action_page.php">
+        <Input name="t5rfidcc" FullName="T5 RFID CC"/>
+        <Input name="hylder" FullName="HYLDER"/>
+        <Input name="forl" FullName="FORL."/>
+        <Input name="rfidcc" FullName="RFID CC"/>
+        <Input name="halfrfidcc" FullName="½ RFID CC"/>
+        <Input name="halfhylde" FullName="½ hylde"/>
+        <Input name="halfcc" FullName="½ CC"/>
+        <Input name="dshalfcc" FullName="DS ½CC"/>
+        <Input name="dshalfhylde" FullName="DS ½HYLDE"/>
+        <Input name="eupl" FullName="EUPL"/>
+        <Input name="halfpll" FullName="½ PLL"/>
+        <Input name="quartpll" FullName="¼ PLL"/>
+        <Input name="cc" FullName="CC"/>
+        <Input name="sojleror" FullName="SØJLERØR"/>
+      </form>
     </div>
   );
 }
 
-
+function Input({name, FullName}) {
+  return (
+    <div className="form-input">
+      <input type="checkbox" name={name} />
+      <label for={name}> {FullName}</label>
+    </div>
+  );
+}
 
 export default DropDown;
