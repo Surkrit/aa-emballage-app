3fd3eb50eaab80ac718eb7cd3dbba38f9eaf3f41
lagfoss <32448248+lagfoss@users.noreply.github.com>
2020-11-13 09:12:26 +0100

    Nesting Test

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import {
    Route,
    Link
  } from "react-router-dom";
-
+ import Prices from './components/Prices';
 
 
 ReactDOM.render(
@@ -27,6 +27,7 @@ ReactDOM.render(
      
          <Switch>
             <Route exact path="/">
+               <Prices />
                <DropDown />
                <Comment comment="Spicy jalapeno bacon ipsum dolor amet cow shank buffalo pig shankle burgdoggen ribeye andouille bacon beef pork turducken kielbasa. Pork loin flank tenderloin porchetta brisket. Spare ribs pork chop corned beef tenderloin chislic pancetta alcatra pork belly rump meatloaf landjaeger doner strip steak."/>
                <Comment comment="Andouille jowl tri-tip spare ribs pastrami ground round. Pork flank spare ribs buffalo strip steak pastrami, tail biltong frankfurter. Ham hock cow tri-tip chislic capicola, tongue kevin. Ground round pork chop doner spare ribs boudin ball tip prosciutto pancetta swine tail alcatra. "/>
