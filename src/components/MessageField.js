43761edaaf603651fc185cc6535bcb10d223ac23
Surkrit <32482770+Surkrit@users.noreply.github.com>
2020-11-23 11:00:04 +0100

    Fixed css på message

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -2,8 +2,11 @@ import '../style/App.css';
 
 function MessageField({message}) {
     return (
-      <textarea className="messagefield" cols="30" rows="10">{message}</textarea>
+      <div className="messagefield--wrapper">
+        <input name="Dato" type="date"/>
+        <textarea className="messagefield" cols="30" rows="10">{message}</textarea>
+      </div>
     );
     }
 
-export default MessageField;
+export default MessageField; 
