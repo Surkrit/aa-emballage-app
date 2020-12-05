12a1f57bb47e8e5a1842c13b9c7ff60e60d50a3e
Katja H. Kjer <Kalle3008@hotmail.com>
2020-12-05 22:02:55 +0100

    Endnu mere tabel, nu med lidt routing

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import { useTable, usePagination } from "react-table";
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import makeData from "./Utils";
 
 import Headline from "./Headline";
@@ -95,12 +97,11 @@ const Styles = styled.div`
     border: 0px solid black !important;
   }
 
-.table-top--buttons{
-  display: flex;
-  justify-content: space-between;
-  margin-top: 20px;
-}
-
+  .table-top--buttons {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 20px;
+  }
 
   .dropdown--button {
     width: 215px;
@@ -115,7 +116,7 @@ const Styles = styled.div`
   }
 
   .dropdown--wrapper {
-    display: none;
+    display: block;
     flex-direction: column;
     width: 213px;
     border: 1px solid black;
@@ -128,12 +129,10 @@ const Styles = styled.div`
 
   .dropdown {
     &:nth-child(-n + 6),
-    &:nth-last-child(-n + 4) {
+    &:nth-last-child(-n + 3) {
       display: none;
     }
   }
-
-  
 
   .pagination {
     margin-top: 20px;
@@ -221,76 +220,151 @@ function Table({ columns, data }) {
         <p>Til dato:</p> <input type="date" />
       </div>
 
+      <Router>
+        <Switch>
+          <Route exact path="/Customer/Home">
+            <h1>Afleveret</h1>
+            <div className="table-top--buttons">
+              <div className="table-buttons">
+                <div className="emballage-buttons">
+                  <button className="button--active">
+                    <Link to="Home">Afleveret/returneret</Link>
+                  </button>
+                  <button>
+                    <Link to="Balance">Balance</Link>
+                  </button>
+                </div>
+              </div>
 
-    <div className="table-top--buttons">
-      <div className="table-buttons">
-        <div className="emballage-buttons">
-          <button className="button--active">
-            <a href="Home">Afleveret/returneret</a>
-          </button>
-          <button>
-            <a href="Balance">Balance</a>
-          </button>
-        </div>
-      </div>
-
-    <div className="filter--wrapper">
-      <button class="dropdown--button">Filtrer</button>
-        <div class="dropdown--wrapper">
-          {allColumns.map((column) => (
-            <div class="dropdown" key={column.id}>
-              <label>
-                <input type="checkbox" {...column.getToggleHiddenProps()} />{" "}
-                {column.id}
-              </label>
+              <div className="filter--wrapper">
+                <button class="dropdown--button">Filtrer</button>
+                <div class="dropdown--wrapper">
+                  {allColumns.map((column) => (
+                    <div class="dropdown" key={column.id}>
+                      <label>
+                        <input
+                          type="checkbox"
+                          {...column.getToggleHiddenProps()}
+                        />{" "}
+                        {column.id}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+              </div>
             </div>
-          ))}
-        </div>
-      </div>
-      </div>
 
-      <table className="emballage-table" {...getTableProps()}>
-        <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {page.map((row, i) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
+            <table className="emballage-table" {...getTableProps()}>
+              <thead>
+                {headerGroups.map((headerGroup) => (
+                  <tr {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map((column) => (
+                      <th {...column.getHeaderProps()}>
+                        {column.render("Header")}
+                      </th>
+                    ))}
+                  </tr>
+                ))}
+              </thead>
+              <tbody {...getTableBodyProps()}>
+                {page.map((row, i) => {
+                  prepareRow(row);
                   return (
-                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    <tr {...row.getRowProps()}>
+                      {row.cells.map((cell) => {
+                        return (
+                          <td {...cell.getCellProps()}>
+                            {cell.render("Cell")}
+                          </td>
+                        );
+                      })}
+                    </tr>
                   );
                 })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+              </tbody>
+            </table>
+          </Route>
 
-      <div className="pagination">
-        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-          {<i class="fas fa-chevron-left"></i>}
-        </button>
+          <Route exact path="/Customer/Balance">
+            <h1>Balance</h1>
+            <div className="table-top--buttons">
+              <div className="table-buttons">
+                <div className="emballage-buttons">
+                  <button>
+                    <Link to="Home">Afleveret/returneret</Link>
+                  </button>
+                  <button className="button--active">
+                    <Link to="Balance">Balance</Link>
+                  </button>
+                </div>
+              </div>
 
-        <span>
-          Side{" "}
-          <strong>
-            {pageIndex + 1} af {pageOptions.length}
-          </strong>{" "}
-        </span>
+              <div className="filter--wrapper">
+                <button class="dropdown--button">Filtrer</button>
+                <div class="dropdown--wrapper">
+                  {allColumns.map((column) => (
+                    <div class="dropdown" key={column.id}>
+                      <label>
+                        <input
+                          type="checkbox"
+                          {...column.getToggleHiddenProps()}
+                        />{" "}
+                        {column.id}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <table className="emballage-table" {...getTableProps()}>
+              <thead>
+                {headerGroups.map((headerGroup) => (
+                  <tr {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map((column) => (
+                      <th {...column.getHeaderProps()}>
+                        {column.render("Header")}
+                      </th>
+                    ))}
+                  </tr>
+                ))}
+              </thead>
+              <tbody {...getTableBodyProps()}>
+                {page.map((row, i) => {
+                  prepareRow(row);
+                  return (
+                    <tr {...row.getRowProps()}>
+                      {row.cells.map((cell) => {
+                        return (
+                          <td {...cell.getCellProps()}>
+                            {cell.render("Cell")}
+                          </td>
+                        );
+                      })}
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </Route>
 
-        <button onClick={() => nextPage()} disabled={!canNextPage}>
-          {<i class="fas fa-chevron-right"></i>}
-        </button>
-      </div>
+          <div className="pagination">
+            <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+              {<i class="fas fa-chevron-left"></i>}
+            </button>
+
+            <span>
+              Side{" "}
+              <strong>
+                {pageIndex + 1} af {pageOptions.length}
+              </strong>{" "}
+            </span>
+
+            <button onClick={() => nextPage()} disabled={!canNextPage}>
+              {<i class="fas fa-chevron-right"></i>}
+            </button>
+          </div>
+        </Switch>
+      </Router>
     </>
   );
 }
