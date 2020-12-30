69176b3b81c4145d08a1649fa06fac20e67305b1
lagfoss <32448248+lagfoss@users.noreply.github.com>
2020-12-30 21:34:10 +0100

    Så er der priser, som ikke kan opdateres :sheep: :ram:

--- src/components/AddRow.js
+++ src/components/AddRow.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-// import Button from './Button';
 import PropTypes from 'prop-types';
 import '../style/App.css';
 
@@ -20,7 +19,6 @@ const initialRow = {
   quartPll: 0,
   cc: 0,
   sojleror: 0,
-  // comment: '',
   subRows: undefined,
 }
 
@@ -42,46 +40,44 @@ const AddRow = props => {
     <div className="add-row">
       <label className="add-row__date">Dato <input type="date" name="date" value={input.date} onChange={handleChange('date')}/></label>
         <div class="table-data">
-        <table>
-          <thead>
-            <tr>
-              <th><h4>T5 Rfid CC</h4></th>
-              <th><h4>Hylder</h4></th>
-              <th><h4>Forl.</h4></th>
-              <th><h4>Rfid CC</h4></th>
-              <th><h4>½ Rfid CC</h4></th>
-              <th><h4>½ Hylde</h4></th>
-              <th><h4>½ CC</h4></th>
-              <th><h4>DS ½CC</h4></th>
-              <th><h4>DS ½Hylde</h4></th>
-              <th><h4>Eupl</h4></th>
-              <th><h4>½ Pll</h4></th>
-              <th><h4>¼ Pll</h4></th>
-              <th><h4>CC</h4></th>
-              <th><h4>Søjlerør</h4></th>
-              {/* <th>Kommentar</th> */}
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td><input type="number" name="t5RfidCc" value={input.t5RfidCc} onChange={handleChange('t5RfidCc')} /></td>
-              <td><input type="number" name="hylder" value={input.hylder} onChange={handleChange('hylder')} /></td>
-              <td><input type="number" name="forl" value={input.forl} onChange={handleChange('forl')} /></td>
-              <td><input type="number" name="rfidCc" value={input.rfidCc} onChange={handleChange('rfidCc')} /></td>
-              <td><input type="number" name="halfRfidCc" value={input.halfRfidCc} onChange={handleChange('halfRfidCc')} /></td>
-              <td><input type="number" name="halfHylde" value={input.halfHylde} onChange={handleChange('halfHylde')} /></td>
-              <td><input type="number" name="halfCc" value={input.halfCc} onChange={handleChange('halfCc')} /></td>
-              <td><input type="number" name="dsHalfCc" value={input.dsHalfCc} onChange={handleChange('dsHalfCc')} /></td>
-              <td><input type="number" name="dsHalfHylde" value={input.dsHalfHylde} onChange={handleChange('dsHalfHylde')} /></td>
-              <td><input type="number" name="eupl" value={input.eupl} onChange={handleChange('eupl')} /></td>
-              <td><input type="number" name="halfPll" value={input.halfPll} onChange={handleChange('halfPll')} /></td>
-              <td><input type="number" name="quartPll" value={input.quartPll} onChange={handleChange('quartPll')} /></td>
-              <td><input type="number" name="cc" value={input.cc} onChange={handleChange('cc')} /></td>
-              <td><input type="number" name="sojleror" value={input.sojleror} onChange={handleChange('sojleror')} /></td>
-              {/* <td><Comment /></td> */}
-            </tr>
-          </tbody>
-        </table>
+          <table>
+            <thead>
+              <tr>
+                <th><h4>T5 Rfid CC</h4></th>
+                <th><h4>Hylder</h4></th>
+                <th><h4>Forl.</h4></th>
+                <th><h4>Rfid CC</h4></th>
+                <th><h4>½ Rfid CC</h4></th>
+                <th><h4>½ Hylde</h4></th>
+                <th><h4>½ CC</h4></th>
+                <th><h4>DS ½CC</h4></th>
+                <th><h4>DS ½Hylde</h4></th>
+                <th><h4>Eupl</h4></th>
+                <th><h4>½ Pll</h4></th>
+                <th><h4>¼ Pll</h4></th>
+                <th><h4>CC</h4></th>
+                <th><h4>Søjlerør</h4></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td><input type="number" name="t5RfidCc" value={input.t5RfidCc} onChange={handleChange('t5RfidCc')} /></td>
+                <td><input type="number" name="hylder" value={input.hylder} onChange={handleChange('hylder')} /></td>
+                <td><input type="number" name="forl" value={input.forl} onChange={handleChange('forl')} /></td>
+                <td><input type="number" name="rfidCc" value={input.rfidCc} onChange={handleChange('rfidCc')} /></td>
+                <td><input type="number" name="halfRfidCc" value={input.halfRfidCc} onChange={handleChange('halfRfidCc')} /></td>
+                <td><input type="number" name="halfHylde" value={input.halfHylde} onChange={handleChange('halfHylde')} /></td>
+                <td><input type="number" name="halfCc" value={input.halfCc} onChange={handleChange('halfCc')} /></td>
+                <td><input type="number" name="dsHalfCc" value={input.dsHalfCc} onChange={handleChange('dsHalfCc')} /></td>
+                <td><input type="number" name="dsHalfHylde" value={input.dsHalfHylde} onChange={handleChange('dsHalfHylde')} /></td>
+                <td><input type="number" name="eupl" value={input.eupl} onChange={handleChange('eupl')} /></td>
+                <td><input type="number" name="halfPll" value={input.halfPll} onChange={handleChange('halfPll')} /></td>
+                <td><input type="number" name="quartPll" value={input.quartPll} onChange={handleChange('quartPll')} /></td>
+                <td><input type="number" name="cc" value={input.cc} onChange={handleChange('cc')} /></td>
+                <td><input type="number" name="sojleror" value={input.sojleror} onChange={handleChange('sojleror')} /></td>
+              </tr>
+            </tbody>
+          </table>
         </div>
         <button onClick={handleAdd} className="button--green">
             {buttonName}
--- src/components/Prices.js
+++ src/components/Prices.js
@@ -1,38 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Headline from './Headline';
 import Button from './Button'
 import '../style/App.css';
 
-function EmbaRow({name, EmbaName}) {
+const initialPrice = {
+  t5RfidCc: 3000,
+  hylder: 200,
+  forl: 20,
+  rfidCc: 1000,
+  halfRfidCc: 600,
+  halfHylde: 120,
+  halfCc: 1500,
+  dsHalfCc: 1200,
+  dsHalfHylde: 100,
+  eupl: 100,
+  halfPll: 70,
+  quartPll: 40,
+  cc: 2000,
+  sojleror: 50,
+}
+
+function EmbaRow({name, EmbaName, onChange, value}) {
   return (
     <tr>
         <td>{EmbaName}</td>
-        <td><input type="number" name={name} /></td>
+        <td><input type="number" name={name} value={value} onChange={onChange}/></td>
         <td>kr pr. stk.</td>
     </tr>
   );
 }
 
 function Prices() {
+  const [input, setInput] = useState(initialPrice)
+
+  const handleChange = name => ({ target: { value } }) => {
+    setInput({ ...input, [name]: value })
+  }
   return (
     <div className="prices">
       <Headline title="Prisliste" />
       <form>
         <table>
-          <EmbaRow EmbaName="T5 Rfid CC" name="t5rfidcc"/>
-          <EmbaRow EmbaName="Hylder" name="hylder"/>
-          <EmbaRow EmbaName="Forl." name="forl"/>
-          <EmbaRow EmbaName="Rfid CC"name="rfidcc"/>
-          <EmbaRow EmbaName="½ Rfid CC" name="½rfidcc"/>
-          <EmbaRow EmbaName="½ hylde" name="½hylde"/>
-          <EmbaRow EmbaName="½ CC" name="½cc"/>
-          <EmbaRow EmbaName="DS ½CC" name="ds½cc"/>
-          <EmbaRow EmbaName="DS ½Hylde" name="ds½hylde"/>
-          <EmbaRow EmbaName="Eupl" name="eupl"/>
-          <EmbaRow EmbaName="½ Pll" name="½pll"/>
-          <EmbaRow EmbaName="¼ Pll" name="¼pll"/>
-          <EmbaRow EmbaName="CC" name="cc"/>
-          <EmbaRow EmbaName="Søjlerør" name="sojleror"/>
+          <tbody>
+            <EmbaRow EmbaName="T5 Rfid CC" name="t5RfidCc" value={input.t5RfidCc} onChange={handleChange('t5RfidCc')}/>
+            <EmbaRow EmbaName="Hylder" name="hylder" value={input.hylder} onChange={handleChange('hylder')}/>
+            <EmbaRow EmbaName="Forl." name="forl" value={input.forl} onChange={handleChange('forl')}/>
+            <EmbaRow EmbaName="Rfid CC"name="rfidCc" value={input.rfidCc} onChange={handleChange('rfidCc')}/>
+            <EmbaRow EmbaName="½ Rfid CC" name="halfRfidCc" value={input.halfRfidCc} onChange={handleChange('halfRfidCc')}/>
+            <EmbaRow EmbaName="½ hylde" name="halfHylde" value={input.halfHylde} onChange={handleChange('halfHylde')}/>
+            <EmbaRow EmbaName="½ CC" name="halfCc" value={input.halfCc} onChange={handleChange('halfCc')}/>
+            <EmbaRow EmbaName="DS ½CC" name="dsHalfCc" value={input.dsHalfCc} onChange={handleChange('dsHalfCc')}/>
+            <EmbaRow EmbaName="DS ½Hylde" name="dsHalfHylde" value={input.dsHalfHylde} onChange={handleChange('dsHalfHylde')}/>
+            <EmbaRow EmbaName="Eupl" name="eupl" value={input.eupl} onChange={handleChange('eupl')}/>
+            <EmbaRow EmbaName="½ Pll" name="halfPll" value={input.halfPll} onChange={handleChange('halfPll')}/>
+            <EmbaRow EmbaName="¼ Pll" name="quartPll" value={input.quartPll} onChange={handleChange('quartPll')}/>
+            <EmbaRow EmbaName="CC" name="cc" value={input.cc} onChange={handleChange('cc')}/>
+            <EmbaRow EmbaName="Søjlerør" name="sojleror" value={input.sojleror} onChange={handleChange('sojleror')}/>
+          </tbody>
         </table>
         <Button buttonname="Opdater"/>
       </form>
